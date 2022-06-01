44d49294b19f9c42aca99af67a58ea45f5ad3491
meetpatel132000 <79510417+meetpatel132000@users.noreply.github.com>
2022-06-01 10:27:00 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ import "./App.css";
 import Quiz from "./component/Quiz";
 import QuizEnd from "./component/QuizEnd";
 
-// **** New Redux Practice ****
-// import { useAppDispatch, useAppSelector } from "./redux/hooks";
-// import { questionsToAsked, addingData } from "./redux/CounterSlice";
 
 function App() {
   
@@ -20,21 +17,3 @@ function App() {
 }
 
 export default App;
-
-// **** New Redux Practice ****
-// const value = useAppSelector((state) => state.counter.value);
-// const dispatch = useAppDispatch();
-// useEffect(() => {
-//   dispatch(questionsToAsked(arr));
-// }, []);
-// const handleChange = () => {
-//   dispatch(fixedAmountIncrement(3));
-// };
-
-// return (
-//   <div className="App">
-
-//     {/*  for practice of new redux */}
-//     {/* <button onClick={handleChange}>count is {value}</button> */}
-//   </div>
-// );
