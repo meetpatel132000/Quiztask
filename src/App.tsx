ac6638dc169907d46c230e3a2e6e797d8c3f15ec
meetpatel132000 <79510417+meetpatel132000@users.noreply.github.com>
2022-06-07 15:59:41 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import "./App.css";
-
+import { useEffect } from "react";
 // components
 import Quiz from "./component/Quiz";
 import QuizEnd from "./component/QuizEnd";
 
+// **** New Redux Practice ****
+import { useAppDispatch } from "./redux/hooks";
+import { questionsToAsked } from "./redux/CounterSlice";
 
 function App() {
-  
+  // Getting array of random numbers
+  const ARRAY_LENGTH: number = 20;
+  let arrayOfRandomNumbers: any = Array.from(Array(ARRAY_LENGTH)).map((x) =>
+    Math.floor(Math.random() * 100)
+  );
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    // storing the array of random numbers
+    dispatch(questionsToAsked(arrayOfRandomNumbers));
+  }, [arrayOfRandomNumbers, dispatch]);
 
   return (
     <div className="App">
