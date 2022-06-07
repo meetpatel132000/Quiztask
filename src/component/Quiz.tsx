b88fd1dca45f277a49ba27baf681e307853198c3
meetpatel132000 <79510417+meetpatel132000@users.noreply.github.com>
2022-06-07 16:00:20 +0530

    Update Quiz.tsx

--- src/component/Quiz.tsx
+++ src/component/Quiz.tsx
@@ -1,28 +1,16 @@
 import "../App.css";
-import { useEffect } from "react";
+
 // Components
 import QuizQuestion from "./QuizQuestion";
 import QuizQuestionButton from "./QuizQuestionButton";
 
 // **** New Redux Practice ****
-import { useAppDispatch, useAppSelector } from "../redux/hooks";
-import { questionsToAsked } from "../redux/CounterSlice";
+import { useAppSelector } from "../redux/hooks";
+
 
 const Quiz = () => {
   const value: any = useAppSelector((state) => state.counter.value);
 
-  // Getting array of random numbers
-  const ARRAY_LENGTH: number = 20;
-  let arrayOfRandomNumbers: any = Array.from(Array(ARRAY_LENGTH)).map((x) =>
-    Math.floor(Math.random() * 100)
-  );
-
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    // storing the array of random numbers
-    dispatch(questionsToAsked(arrayOfRandomNumbers));
-  }, [arrayOfRandomNumbers, dispatch]);
 
   return (
     <div>
